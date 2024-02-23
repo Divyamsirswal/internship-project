fc92274d939e605ad63ab51ec87048641bf4a788
Meet <meetmendapara09@gmail.com>
2024-02-23 20:27:24 +0530

    update the file dependencies.

--- src/assets/Boxes/Header.jsx
+++ src/assets/Boxes/Header.jsx
@@ -2,12 +2,12 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../../../public/ICONS/divinelogohorizontal.png";
-import { InputGroup, Form } from 'react-bootstrap';
-import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
-import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
-import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
-import CasinoRoundedIcon from '@mui/icons-material/CasinoRounded';
+import { InputGroup, Form } from "react-bootstrap";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import CasinoRoundedIcon from "@mui/icons-material/CasinoRounded";
 
 function Header() {
   return (
@@ -15,18 +15,22 @@ function Header() {
       <Container fluid>
         <Navbar.Brand href="#home">
           <img
-          className="ms-3"
+            className="ms-3"
             src={logo}
             alt="Divine Logo"
-            style={{ height: "auto", maxHeight: "80px", width: "auto" }} 
+            style={{ height: "auto", maxHeight: "80px", width: "auto" }}
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home" className="me-3">Todays Luck <CasinoRoundedIcon/></Nav.Link>
-            <Nav.Link href="#link" className="me-3">Shopping <ShoppingCartRoundedIcon/></Nav.Link>
-            
+            <Nav.Link href="#home" className="me-3">
+              Todays Luck <CasinoRoundedIcon />
+            </Nav.Link>
+            <Nav.Link href="#link" className="me-3">
+              Shopping <ShoppingCartRoundedIcon />
+            </Nav.Link>
+
             <InputGroup className="mb-1 me-3" style={{ width: "300px" }}>
               <Form.Control
                 placeholder="Search Your Divine Connection"
@@ -34,7 +38,7 @@ function Header() {
                 aria-describedby="basic-addon2"
               />
               <InputGroup.Text id="basic-addon2">
-                <SearchRoundedIcon/>
+                <SearchRoundedIcon />
               </InputGroup.Text>
             </InputGroup>
 
@@ -45,10 +49,15 @@ function Header() {
               <option value="de">German</option>
             </select>
 
-            <Nav.Link href="#home" className="me-3">Login/Registration</Nav.Link>
-            <Nav.Link href="#link" className="me-3"><AccountCircleRoundedIcon/></Nav.Link>
-            <Nav.Link href="#link" className="me-3"><NotificationsRoundedIcon/></Nav.Link>
-
+            <Nav.Link href="#home" className="me-3">
+              Login/Registration
+            </Nav.Link>
+            <Nav.Link href="#link" className="me-3">
+              <AccountCircleRoundedIcon />
+            </Nav.Link>
+            <Nav.Link href="#link" className="me-3">
+              <NotificationsRoundedIcon />
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
