599aa803962c5fb6e77e4fea791a9914595f0dbd
Meet <meetmendapara09@gmail.com>
2024-02-23 10:12:38 +0530

    CSS file Minor changes

--- src/assets/Boxes/FutureBoxes.jsx
+++ src/assets/Boxes/FutureBoxes.jsx
@@ -7,72 +7,72 @@ function FutureBoxes() {
         <h2>Read Your Future Now</h2>
         <div className="box">
           <img src="image1.jpg" alt="Image 1" />
-          <h3>Box 1</h3>
+          <h3>Tarot Reading</h3>
         </div>
         <div className="box">
           <img src="image2.jpg" alt="Image 2" />
-          <h3>Box 2</h3>
+          <h3>Angel Reader</h3>
         </div>
         <div className="box">
           <img src="image3.jpg" alt="Image 3" />
-          <h3>Box 3</h3>
+          <h3>katina Love Reading</h3>
         </div>
         <div className="box">
           <img src="image4.jpg" alt="Image 4" />
-          <h3>Box 4</h3>
+          <h3>Face Reader</h3>
         </div>
         <div className="box">
           <img src="image5.jpg" alt="Image 5" />
-          <h3>Box 5</h3>
+          <h3>Palm Reader</h3>
         </div>
         <div className="box">
           <img src="image6.jpg" alt="Image 6" />
-          <h3>Box 6</h3>
+          <h3>Finger Reader</h3>
         </div>
       </div>
       <div className="section">
         <h2>Embark on your Journey</h2>
         <div className="box">
           <img src="image7.jpg" alt="Image 7" />
-          <h3>Box 7</h3>
+          <h3>Time Travel</h3>
         </div>
         <div className="box">
-          <img src="image8.jpg" alt="Image 8" />
-          <h3>Box 8</h3>
+          <img src="/icons/astrology.png" alt="Image 8" />
+          <h3>Astrology</h3>
         </div>
         <div className="box">
           <img src="image9.jpg" alt="Image 9" />
-          <h3>Box 9</h3>
+          <h3>Biorythm</h3>
         </div>
       </div>
       <div className="section">
         <h2>Lets take you to the another Dimension</h2>
         <div className="box">
           <img src="image10.jpg" alt="Image 10" />
-          <h3>Box 10</h3>
+          <h3>Dream Interpretation</h3>
         </div>
         <div className="box">
           <img src="image11.jpg" alt="Image 11" />
-          <h3>Box 11</h3>
+          <h3>Crystal Ball</h3>
         </div>
         <div className="box">
           <img src="image12.jpg" alt="Image 12" />
-          <h3>Box 12</h3>
+          <h3>Numerlogy</h3>
         </div>
       </div>
       <div className="section">
         <h2>Nourish Your soul</h2>
         <div className="box">
           <img src="image13.jpg" alt="Image 13" />
-          <h3>Box 13</h3>
+          <h3>Medditation</h3>
         </div>
         <div className="box">
           <img src="image14.jpg" alt="Image 14" />
-          <h3>Box 14</h3>
+          <h3>Emotional Support</h3>
         </div>
         <div className="box">
           <img src="image15.jpg" alt="Image 15" />
-          <h3>Box 15</h3>
+          <h3>Get Your Solutions</h3>
         </div>
       </div>
     </div>
