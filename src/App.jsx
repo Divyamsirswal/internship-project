abe9ccde9a14f45906fe87f74f13ef3d7c05a87f
divyamTest <divyamsirswal361@gmail.com>
2024-02-25 13:57:30 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,5 @@
 import { Container } from "react-bootstrap";
 import FutureBoxes from "./assets/Boxes/FutureBoxes";
-<<<<<<< HEAD
-import Header from "./assets/Boxes/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TarotReading from "./pages/TarotReading";
-import AngelReading from "./pages/AngelReading";
-import KetinaLoveReading from "./pages/KetinaLoveReading";
-
-function App() {
-  return (
-    <Router>
-      <Container>
-        <div className="App">
-          <Header />
-          <Routes>
-            <Route path="/" element={<FutureBoxes />} />
-            <Route path="/tarot_reading" element={<TarotReading />} />
-            <Route path="/angel_reading" element={<AngelReading />} />
-            <Route
-              path="/ketina_love_reading"
-              element={<KetinaLoveReading />}
-            />
-          </Routes>
-        </div>
-      </Container>
-    </Router>
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./assets/Boxes/Header";
 import Meditation from "./components/meditation/Meditation";
@@ -38,7 +12,6 @@ function App() {
         <Route path="/Meditation" element={<Meditation />} />
       </Routes>
     </BrowserRouter>
->>>>>>> eda943a03464a0108ad77f5fee660cc78940b4cf
   );
 }
 
