7ffe3801c8baeda7f113967238264df9cf8d73b8
divyamTest <divyamsirswal361@gmail.com>
2024-02-26 16:43:41 +0530

    changes in the files

--- src/Page/TarotReading/TarotReading.jsx
+++ src/Page/TarotReading/TarotReading.jsx
@@ -3,23 +3,30 @@ const TarotReading = () => {
     <div className="flex flex-col h-full">
       <div className="flex-grow w-screen h-screen flex flex-col  bg-red-200">
         <div className="text-center h-max w-full p-10">
-          <span className="text-4xl">Tarot Reading</span>
+          <span className="text-4xl font-mono">Tarot Reading</span>
         </div>
-        <div className="p-20 gap-20 items-center justify-center flex w-screen h-max">
+        <div className="p-10 gap-20 items-center justify-center flex w-screen h-max">
           <div className=" flex gap-20">
-            <div className="bg-red-400 w-[500px] h-[250px] rounded-3xl"></div>
-            <div className="bg-red-400 w-[500px] h-[250px] rounded-3xl"></div>
+            <div className="bg-red-100 w-[500px] h-[250px] relative rounded-3xl shadow outline outline-2 cursor-pointer">
+              <div className=" bg-black w-[500px] h-[110px] rounded-b-3xl absolute bottom-0 "></div>
+            </div>
+            <div className="bg-red-100 w-[500px] h-[250px] relative rounded-3xl shadow outline outline-2 cursor-pointer">
+              <div className=" bg-black w-[500px] h-[110px] rounded-b-3xl absolute bottom-0 "></div>
+            </div>
           </div>
         </div>
-        <div className="px-20 items-center justify-center py-10 gap-20 flex w-screen h-max">
+        <div className=" items-center justify-center p-10 gap-20 flex w-screen h-max">
           <div className=" flex gap-20">
-            <div className="bg-red-400 w-[500px] h-[250px] rounded-3xl"></div>
-            <div className="bg-red-400 w-[500px] h-[250px] rounded-3xl"></div>
+            <div className="bg-red-100 w-[500px] relative h-[250px] rounded-3xl shadow outline outline-2 cursor-pointer">
+              <div className=" bg-black w-[500px] h-[110px] rounded-b-3xl absolute bottom-0 "></div>
+            </div>
+            <div className="bg-red-100 w-[500px] relative h-[250px] rounded-3xl shadow outline outline-2 cursor-pointer">
+              <div className=" bg-black w-[500px] h-[110px] rounded-b-3xl absolute bottom-0 "></div>
+            </div>
           </div>
         </div>
       </div>
       <div className="flex-grow w-screen h-screen flex items-center justify-center bg-green-200"></div>
-      <div className="flex-grow w-screen h-screen flex items-center justify-center bg-blue-200"></div>
     </div>
   );
 };
