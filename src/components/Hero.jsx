7699aba229148c7b1d226578a09c11f81ae14267
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2024-02-26 21:12:49 +0530

    Ajay's commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,13 @@ const Hero = () => {
     <div class="size-full bg-gradient-to-r from-purple-300 to-violet-200 shadow-lg rounded-lg p-5 dark:bg-slate-900">
       <div class="flex items-center gap-x-4 mb-3">
         {/* <div class="inline-flex justify-center items-center size-[62px] rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-          <svg class="flex-shrink-0 size-6 text-blue-600 dark:text-blue-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"/><circle cx="17.5" cy="10.5" r=".5"/><circle cx="8.5" cy="7.5" r=".5"/><circle cx="6.5" cy="12.5" r=".5"/><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"/></svg>
+
         </div> */}
         <div class="flex-shrink-0">
           <h3 class="block text-lg font-semibold text-gray-800 dark:text-white">Daily Horrorscopes</h3>
         </div>
       </div>
-      {/* <p class="text-gray-600 dark:text-gray-400">The simplest way to keep your portfolio always up-to-date.</p> */}
+     
     </div>
   
 
@@ -24,13 +24,13 @@ const Hero = () => {
     <div class="size-full bg-gradient-to-r from-purple-300 to-violet-200 shadow-lg rounded-lg p-5 dark:bg-slate-900">
       <div class="flex items-center gap-x-4 mb-3">
         {/* <div class="inline-flex justify-center items-center size-[62px] rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-          <svg class="flex-shrink-0 size-6 text-blue-600 dark:text-blue-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 3h20"/><path d="M21 3v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V3"/><path d="m7 21 5-5 5 5"/></svg>
+
         </div> */}
         <div class="flex-shrink-0">
           <h3 class="block text-lg font-semibold text-gray-800 dark:text-white">Kundali</h3>
         </div>
       </div>
-      {/* <p class="text-gray-600 dark:text-gray-400">New design projects delivered to your inbox each morning.</p> */}
+     
     </div>
 
 
@@ -38,26 +38,26 @@ const Hero = () => {
     <div class="size-full shadow-lg rounded-lg p-5 bg-gradient-to-r from-purple-300 to-violet-200 dark:bg-slate-900">
       <div class="flex items-center gap-x-4 mb-3">
         {/* <div class="inline-flex justify-center items-center size-[62px] rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-          <svg class="flex-shrink-0 size-6 text-blue-600 dark:text-blue-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m2 7 4.41-4.41A2 2 0 0 1 7.83 2h8.34a2 2 0 0 1 1.42.59L22 7"/><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"/><path d="M15 22v-4a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v4"/><path d="M2 7h20"/><path d="M22 7v3a2 2 0 0 1-2 2v0a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 16 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 12 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 8 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 4 12v0a2 2 0 0 1-2-2V7"/></svg>
+          
         </div> */}
         <div class="flex-shrink-0">
           <h3 class="block text-lg font-semibold text-gray-800 dark:text-white">Love check</h3>
         </div>
       </div>
-      {/* <p class="text-gray-600 dark:text-gray-400">Get your goods in front of millions of potential customers with ease.</p> */}
+      
     </div>
 
 
     <div class="size-full shadow-lg rounded-lg p-5 bg-gradient-to-r from-purple-300 to-violet-200 dark:bg-slate-900">
       <div class="flex items-center gap-x-4 mb-3">
         {/* <div class="inline-flex justify-center items-center size-[62px] rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-          <svg class="flex-shrink-0 size-6 text-blue-600 dark:text-blue-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m2 7 4.41-4.41A2 2 0 0 1 7.83 2h8.34a2 2 0 0 1 1.42.59L22 7"/><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"/><path d="M15 22v-4a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v4"/><path d="M2 7h20"/><path d="M22 7v3a2 2 0 0 1-2 2v0a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 16 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 12 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 8 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 4 12v0a2 2 0 0 1-2-2V7"/></svg>
+          
         </div> */}
         <div class="flex-shrink-0">
           <h3 class="block text-lg font-semibold text-gray-800 dark:text-white">Fortune Wheel</h3>
         </div>
       </div>
-      {/* <p class="text-gray-600 dark:text-gray-400">Get your goods in front of millions of potential customers with ease.</p> */}
+      
     </div>
   
   </div>
