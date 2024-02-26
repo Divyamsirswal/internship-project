376fabcfeb1a6154a5a380bed2d3fd7ab846e6f4
unknown <jaybhuptani1054@gmail.com>
2024-02-26 10:33:57 +0530

    added dailymeditation

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./assets/Boxes/Header";
 import Meditation from "./components/meditation/Meditation";
 import Home from "./Page/Home/Home";
+import DailyMeditation from "./components/meditation/DailyMeditation";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Meditation" element={<Meditation />} />
+        <Route path="/DailyMeditation" element={<DailyMeditation />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/meditation/Meditation.jsx
+++ src/components/meditation/Meditation.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import "./meditation.css";
 
 const Meditation = () => {
+  const navigate = useNavigate();
   const options = [
     "Daily Meditation",
     "Chakra Meditation",
@@ -27,6 +28,12 @@ const Meditation = () => {
     "https://images.unsplash.com/photo-1559595500-e15296bdbb48?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1554067559-269708c83fb6?q=80&w=1994&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
+  const onButtonClick = (option) => {
+    if (option === "Daily Meditation") {
+      navigate("/DailyMeditation");
+      console.log("Daily Meditation");
+    }
+  };
   return (
     <div className="Main-page">
       <div className="Meditation-text">
@@ -38,7 +45,12 @@ const Meditation = () => {
           return (
             <div className="list">
               <div className="circle">
-                <img className="hy" src={imgurl[index]} alt="Meditation" />
+                <img
+                  className="hy"
+                  onClick={() => onButtonClick(option)}
+                  src={imgurl[index]}
+                  alt="Meditation"
+                />
               </div>
               {/* <div className="circle secpic">
                 <img src={imgurl[index]} alt="Meditation" />
