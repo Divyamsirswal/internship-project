e2d1e7d58960a6f118d37d65f81fe7994eb96ba0
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2024-02-26 21:08:10 +0530

    Ajay's commit

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,12 +1,14 @@
 import { Container } from "react-bootstrap";
 import FutureBoxes from "../../assets/Boxes/FutureBoxes";
 import Header from "../../assets/Boxes/Header";
+import Hero from "../../components/Hero";
 
 const Home = () => {
   return (
     <Container>
       <div className="App">
         <Header />
+        <Hero />
         <FutureBoxes />
       </div>
     </Container>
